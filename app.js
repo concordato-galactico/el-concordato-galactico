f9089e685f00624dbd293dcaac223d08eb3b37e3
concordato-galactico <concordato@proton.me>
2026-03-29 12:44:03 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -178,7 +178,7 @@ function añadirMarcaAlMapa(marca) {
   grupoPins.addLayer(marker);
   markersPorId[marca.id] = marker;
 
-  const etiqueta = L.tooltip({ permanent: true, direction: 'top', offset: [0, -18] })
+  const etiqueta = L.tooltip({ permanent: true, direction: 'top', offset: [0, -44] })
     .setContent(marca.nombre)
     .setLatLng([marca.lat, marca.lng]);
   grupoNombres.addLayer(etiqueta);
